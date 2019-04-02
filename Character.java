5b849718754397aedf27618b9189a7d066ecc73f
ametuk8025 <ametuk8025@CHHB2ZLKXV1.inst.hcpss.org>
2019-04-02 13:09:45 -0400

    4/2/19

--- Character.java
+++ Character.java
@@ -1,4 +1,32 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.event.*;
+public class Character implements ActionListener {
+	
+	static JLayeredPane EverythingButPlayer;
+	static Character l1;
+	
+	public Character() {
+	    EverythingButPlayer = new JLayeredPane();
 
-public class Character {
+	    BufferedImage img = null;
+	    try {
+	        img = ImageIO.read(new File("BeachRoad.png"));
+	    } catch (IOException e) {
+	    }
+	    Graphics g = img.getGraphics();
+	    g.drawImage(img, 0, 0, null);
 
+	   
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
