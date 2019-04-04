3aa55b570e81757a48c2fbaa44055d40d3c41bf1
harun <harun@LAPTOP-J0LIJ2J7.fios-router.home>
2019-04-03 20:47:44 -0400

    almost got this working

--- Map.java
+++ Map.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.awt.event.*;
 
-public class Map extends JPanel implements WindowListener{
+public class Map extends JPanel{
 
 	JFrame frame;
 	ArrayList<Character> characters;
@@ -31,18 +31,18 @@ public class Map extends JPanel implements WindowListener{
 		
 		
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.addWindowListener(this);
 		frame.setSize(width,height);
 		this.setSize(width,height);
 		Graphics g = frame.getGraphics();
 		frame.add(singleton);
-		singleton.paintComponents(g); 
-		frame.setVisible(false);
+		singleton.paintComponents(g);
+		frame.setVisible(true);
+//		System.out.println("does this");
 	}
 	//Call this to call the bottom paintComponent class code and update the map
-	public void updateMap() {
+	public void updateMap() {//Does not call paintComponent
 		Graphics g = frame.getGraphics();
-		singleton.paintComponents(g);
+		singleton.paint(g);
 		frame.setVisible(true);
 	}
 
@@ -52,7 +52,7 @@ public class Map extends JPanel implements WindowListener{
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		System.out.println("goes herer");
+//		System.out.println("goes herer");
 
 		for(Character character:characters) {
 			character.updatePosition();
@@ -68,84 +68,19 @@ public class Map extends JPanel implements WindowListener{
 	}
 	
 	public void mainLoop() {//need to create a consistent game loop : (used fixed frame rates) http://www.java-gaming.org/index.php?topic=24220.0
-		loop = true;
-		//Try 1
-//		while(loop) {
-//			updateMap();
-//			
-//			if(characters.isEmpty())
-//				loop = false;
-//			if(closing) // this loop prevents the closing of the frame
-//				loop = false;
-//		}
-		
-		//Test Try
-//		for(int i= 0 ;i < 10000; i++ ) {
-//			updateMap();
-//		}
-		
-		//Another attempt a game loop
-//		long lastLoopTime = System.nanoTime();
-//		   final int TARGET_FPS = 60;
-//		   final long OPTIMAL_TIME = 1000000000 / TARGET_FPS;
-//		   long lastFpsTime = 0;
-//		   long fps = 0;
-//		   
-//
-//		   // keep looping round til the game ends
-//		   while (loop){
-//			   updateMap();
-//			   singleton.repaint();
-//			   try {
-//				Thread.sleep(10);
+		loop = true;		// also need to create a new thread to run this
+		while(loop) {
+			updateMap();
+//			try {
+//				Thread.sleep(100);
 //			} catch (InterruptedException e) {
 //				// TODO Auto-generated catch block
 //				e.printStackTrace();
 //			}
-//		   }	
-	}
-
-	@Override
-	public void windowActivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void windowClosed(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void windowClosing(WindowEvent e) {
-		// TODO Auto-generated method stub
-		closing = true;
-	}
-
-	@Override
-	public void windowDeactivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void windowDeiconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void windowIconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void windowOpened(WindowEvent e) {
-		// TODO Auto-generated method stub
+		}
 		
 	}
 
 }
-
+	
+	
--- Player.java
+++ Player.java
@@ -1,13 +1,14 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
-public class Player extends Character implements ActionListener{
+public class Player extends Character implements KeyListener{
 	
 	String imageAddress = "C:\\Users\\harun\\Desktop\\Example.jpg"; // Change this when back in school
 	
@@ -42,6 +43,7 @@ public class Player extends Character implements ActionListener{
 
 	        if (buttonPressed == KeyEvent.VK_LEFT) {
 	            xComponent-=hM;
+	            System.out.println("does this");
 	        }
 
 	        if (buttonPressed == KeyEvent.VK_RIGHT) {
@@ -56,16 +58,24 @@ public class Player extends Character implements ActionListener{
 	        	yComponent-=vM;
 	        }
 	    }
+
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void updatePosition() {
+		// TODO Auto-generated method stub
+		xComponent +=1;
+		yComponent +=1;
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void updatePosition() {
+	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
 		
 	}
 
