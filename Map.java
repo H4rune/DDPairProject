9ec637e9a68ce5d2118aef9187ea35033499c686
harun <harun@LAPTOP-J0LIJ2J7.fios-router.home>
2019-04-03 23:24:24 -0400

    fuuuuuuuuuuu

--- Map.java
+++ Map.java
@@ -9,13 +9,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.awt.event.*;
 
-public class Map extends JPanel{
+public class Map extends JPanel implements Runnable{
 
 	JFrame frame;
 	ArrayList<Character> characters;
 	boolean loop;
 	boolean closing;
 	Map singleton;
+	Thread thread;
 	
 	
 
@@ -32,7 +33,7 @@ public class Map extends JPanel{
 		
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setSize(width,height);
-		this.setSize(width,height);
+		singleton.setSize(width,height);
 		Graphics g = frame.getGraphics();
 		frame.add(singleton);
 		singleton.paintComponents(g);
@@ -66,20 +67,103 @@ public class Map extends JPanel{
 		}
 
 	}
-	
 	public void mainLoop() {//need to create a consistent game loop : (used fixed frame rates) http://www.java-gaming.org/index.php?topic=24220.0
-		loop = true;		// also need to create a new thread to run this
-		while(loop) {
-			updateMap();
-//			try {
-//				Thread.sleep(100);
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
+		//now in gameLoop
+		thread = new Thread(singleton,"test");
+		thread.start();
 		}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		boolean loop = true;
+		long lastLoopTime = System.nanoTime();
+		final int TARGET_FPS = 20;
+		final long OPTIMAL_TIME = 1000000000 / TARGET_FPS;   
+		long fps = 0;
+		long lastFpsTime = 0;
+
+		   // keep looping round til the game ends
+		   while (loop)
+		   {
+		      // work out how long its been since the last update, this
+		      // will be used to calculate how far the entities should
+		      // move this loop
+		      long now = System.nanoTime();
+		      long updateLength = now - lastLoopTime;
+		      lastLoopTime = now;
+		      double delta = updateLength / ((double)OPTIMAL_TIME);
+
+		      // update the frame counter
+		      lastFpsTime += updateLength;
+		      fps++;
+		      
+		      // update our FPS counter if a second has passed since
+		      // we last recorded
+		      if (lastFpsTime >= 1000000000)
+		      {
+		         System.out.println("(FPS: "+fps+")");
+		         lastFpsTime = 0;
+		         fps = 0;
+		      }
+		      
+		      try {
+		    	  updateMap();
+		      }
+		      catch(Exception e) {
+//		    	  System.out.println("LOLOLOLOLOL");
+//		    	  thread.interrupt();
+		    	  thread.stop();
+//		    	  thread.destroy();
+		      }
+		      
+		      // we want each frame to take 10 milliseconds, to do this
+		      // we've recorded when we started the frame. We add 10 milliseconds
+		      // to this and then factor in the current time to give 
+		      // us our final value to wait for
+		      // remember this is in ms, whereas our lastLoopTime etc. vars are in ns.
+		      try{Thread.sleep( (lastLoopTime-System.nanoTime() + OPTIMAL_TIME)/1000000 );}
+		      catch(Exception e) {
+		    	  
+		      }
+		   }
+		}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		int buttonPressed = e.getKeyCode();
+
+        if (buttonPressed == KeyEvent.VK_LEFT) {
+            System.out.println("sfkldsjfkldsjfs");
+        }
+
+        if (buttonPressed == KeyEvent.VK_RIGHT) {
+        	System.out.println("shjfdsfdfs");
+        }
+
+        if (buttonPressed == KeyEvent.VK_UP) {
+        	System.out.println("jdklsjfdsklf");
+        }
+
+        if (buttonPressed == KeyEvent.VK_DOWN) {
+        	System.out.println("sfdsfsdfds");
+        }
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+
 
 }
 	
