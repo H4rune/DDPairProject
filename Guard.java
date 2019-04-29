788693bc1066f1484e0ca06fcddf6180fc6ebe20
ametuk8025 <ametuk8025@CHHB2ZLKXV1.inst.hcpss.org>
2019-04-29 13:09:47 -0400

    wwew

--- Guard.java
+++ Guard.java
@@ -1,19 +1,20 @@
 public abstract class Guard extends Character {
+	int lastIndexReached;
 	int index;
-	String FacingDirection = "";
+	String facingDirection = "";// "up" "down" "left" "right"
 	int[][] Coordinates /*= {{2,2},{2,2}}*/;
-	boolean reachedPoint = false;
-
-	int[] pointToReach = {400,400};
-
 
 	String imageAddress = "";
 
 	Guard(String imageFilePath){
 		imageAddress = imageFilePath;
 		setCharacterImage(imageAddress);
+		lastIndexReached= 0;
 	}
-
+	public void setStartingPoint(int x, int y) {
+		xComponent = x;
+		yComponent = y; 		
+	}
 	public void setLookingDirectionVector() {}
 
 	public void setCollisionReaction() {}
@@ -26,13 +27,63 @@ public abstract class Guard extends Character {
 		//		}
 
 		int xCor = this.getX();
-		if(xCor < 200) {
-			xComponent += hM/10;
-			String FacingDirection = "right";
+		int yCor = this.getY();
+
+		if(Coordinates[lastIndexReached][0] == xCor) {
+			if(Coordinates[lastIndexReached][1] > yCor) {
+				facingDirection = "down" ;
+			}
+			if(Coordinates[lastIndexReached][1] < yCor) {
+				facingDirection = "up" ;
+			}
 		}
-		if(xCor >= 200 && xCor<205) {
+		if(Coordinates[lastIndexReached][0] == yCor) {
+			if(Coordinates[lastIndexReached][0] > xCor) {
+				facingDirection = "left" ;
+			}
+			if(Coordinates[lastIndexReached][0] < xCor) {
+				facingDirection = "right" ;
+			}
+		}
+		
+		if(facingDirection.equals("up")) {
+			
+		}
+		if(facingDirection.equals("down")) {
 			yComponent += vM/10;
-			String FacingDirection = "down";
+		}
+		if(facingDirection.equals("left")) {
+			if(Coordinates[lastIndexReached+1][0] - xCor <10) {
+				xComponent += Coordinates[lastIndexReached+1][0] - xCor;
+			}else {
+			xComponent -= hM/10;//work here bext tine
+			}
+		}
+		if(facingDirection.equals("right")) {
+			if(Coordinates[lastIndexReached+1][0] - xCor <10) {
+				xComponent += Coordinates[lastIndexReached+1][0] - xCor;
+			}else {
+			xComponent += hM/10;
+			}
+		}
+		//		if(xCor < 200) {
+		//			xComponent += hM/10;
+		//			String FacingDirection = "right";
+		//		}
+		//		if(xCor >= 200 && xCor<205) {
+		//			yComponent += vM/10;
+		//			String FacingDirection = "down";
+		//		}
+
+
+
+
+
+	}
+
+	public void followPoint(int[] point) {
+		if(this.getX()<point[0]) {
+
 		}
 	}
 
@@ -41,7 +92,7 @@ public abstract class Guard extends Character {
 		int yC = 1;
 		int xCor = this.getX();
 		int yCor = this.getY();
-		
+
 		Coordinates = points;
 		//		for(int i = 0; i<Coordinates.length; i++) {
 		if(xCor < Coordinates[index][xC]) {
