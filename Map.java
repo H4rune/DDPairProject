3ada57158fd0cf76fa1b6ee4c48e3d3453f347be
ametuk8025 <ametuk8025@CHHB2ZLKXV1.inst.hcpss.org>
2019-05-13 12:33:06 -0400

    Guard Vision fully works now

--- Map.java
+++ Map.java
@@ -1,3 +1,4 @@
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
@@ -28,11 +29,11 @@ public class Map extends JPanel implements Runnable, KeyListener{
 	int dx = 0;
 	int dy = 0;
 	Point cursor;
-	
-	
+
+
 
 	public void setBackground() {
-		
+
 	}
 
 	Map(int width, int height){
@@ -40,7 +41,7 @@ public class Map extends JPanel implements Runnable, KeyListener{
 		characters = new ArrayList<Character>();
 		closing = false;
 		singleton = this;
-		
+
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setSize(width,height);
 		singleton.setSize(width,height);
@@ -48,17 +49,17 @@ public class Map extends JPanel implements Runnable, KeyListener{
 		frame.add(singleton);
 		singleton.paintComponents(g);
 		frame.setVisible(true);
-		
+
 		frame.addKeyListener(this);
-		
-//		System.out.println("does this");
+
+		//		System.out.println("does this");
 	}
 	//Call this to call the bottom paintComponent class code and update the map
 	public void updateMap() {//Does not call paintComponent
 		Graphics g = singleton.getGraphics();
 		singleton.paintComponent(g);
-//		frame.setVisible(true);
-		
+		//		frame.setVisible(true);
+
 	}
 
 	public void addCharacter(Character character) {
@@ -67,14 +68,14 @@ public class Map extends JPanel implements Runnable, KeyListener{
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-//		System.out.println("goes here");
-		
+		//		System.out.println("goes here");
+
 		Graphics2D g2= (Graphics2D) g;
 		ArrayList<Rectangle> hitboxes = new ArrayList<Rectangle>();
-		
-		
+
+
 		for(Character character:characters) {
-			
+
 			//Calculating image movement below
 			character.setMovement(dx, dy);
 			Point framePoint = frame.getLocation();
@@ -84,25 +85,25 @@ public class Map extends JPanel implements Runnable, KeyListener{
 			character.setCursorPoint(cursor);
 			character.setCursorAngle();
 			character.updatePosition();
-			
+
 			//Coordinates of the center of the image
 			int xCenter = character.getX();
 			int yCenter = character.getY();
-			
+
 			//Coordinates of the top left of the image
 			//uses offset to make it look like it is rotating about its center
 			int xPos = xCenter - character.getXOffset();
 			int yPos = yCenter - character.getYOffset();
-			
-			
-			
+
+
+
 			//Calculate and draw hitbox before actual image
 			character.setHitbox(xCenter, yCenter, character.getWidth(), character.getHeight());
 			g.drawRect((int)character.getHitbox().getX(), (int)character.getHitbox().getY(), (int)character.getHitbox().getWidth(), (int)character.getHitbox().getHeight());
-			
+
 			character.updateHitbox();
-			
-			
+
+
 			hitboxes.clear();
 			for(Character CBox:characters) {
 				if(!character.hitbox.equals(CBox.getHitbox())) {
@@ -115,61 +116,59 @@ public class Map extends JPanel implements Runnable, KeyListener{
 				}
 				else {
 					//update position here
-//					character.updatePosition();
-					
+					//					character.updatePosition();
+
 					//Draw image
 					Image image = character.getImage();
-					
+
 					double angle = character.getAngle();
-					
+
 					g2.rotate(angle, xCenter, yCenter);
 					g.drawImage(image, xPos, yPos, null);
 					g2.rotate(-angle, xCenter, yCenter);
 				}
 			}
-			
-			
+
+
 			//Draw image
-//			Image image = character.getImage();
-//			
-//			double angle = character.getAngle();
-//			
-//			g2.rotate(angle, xCenter, yCenter);
-//			g.drawImage(image, xPos, yPos, null);
-//			g2.rotate(-angle, xCenter, yCenter);
+			//			Image image = character.getImage();
+			//			
+			//			double angle = character.getAngle();
+			//			
+			//			g2.rotate(angle, xCenter, yCenter);
+			//			g.drawImage(image, xPos, yPos, null);
+			//			g2.rotate(-angle, xCenter, yCenter);
 
 			if(character instanceof Guard) {
-				int playerX = characters.get(0).getX();
-				int playerY = characters.get(0).getY();
-				((Guard) character).canHeSeeThis(playerX, playerY);
-//				int guardX = character.getX();
-//				int guardY = character.getY();
-				
-				//do the stuff here
+				Character thePlayer = characters.get(0);
+				int playerX = thePlayer.getX();
+				int playerY = thePlayer.getY();
+				if(((Guard) character).canHeSeeThis(playerX, playerY)) {
+					thePlayer.setDead();
+					System.out.println("player is dead");
+				}
+//				checkForDeadCharacters(character);
 			}
 		}
 
 	}
-	
+
+
 	public void checkForDeadCharacters(Character character) {
-		
-		if(character instanceof Guard) {
-			int playerX = characters.get(0).getX();
-			int playerY = characters.get(0).getY();
-			
-			((Guard) character).canHeSeeThis(playerX, playerY);
-//			int guardX = character.getX();
-//			int guardY = character.getY();
-			
-			//do the stuff here
-		}
+//		Character thePlayer = characters.get(0);
+//		int playerX = thePlayer.getX();
+//		int playerY = thePlayer.getY();
+//		if(((Guard) character).canHeSeeThis(playerX, playerY)) {
+//			thePlayer.setDead();
+//			System.out.println("player is dead");
+//		}
 	}
-	
+
 	public void mainLoop() {//need to create a consistent game loop : (used fixed frame rates) http://www.java-gaming.org/index.php?topic=24220.0
 		//now in gameLoop
 		thread = new Thread(singleton,"test");
 		thread.start();
-		}
+	}
 
 	@Override
 	public void run() {
@@ -181,59 +180,59 @@ public class Map extends JPanel implements Runnable, KeyListener{
 		long fps = 0;
 		long lastFpsTime = 0;
 
-		   // keep looping round til the game ends
-		   while (loop)
-		   {
-		      // work out how long its been since the last update, this
-		      // will be used to calculate how far the entities should
-		      // move this loop
-		      long st = System.nanoTime();
-		      
-		      try {
-		    	  updateMap();
-		      }
-		      catch(Exception e) {
-		    	  thread.stop();
-		      }
-		      
-		      
-		      while ((System.nanoTime() - st) < 1_000_000_000 / TARGET_FPS);
-		      
-		      // update the frame counter
-		      fps++;
-		      
-		      // update our FPS counter if a second has passed since
-		      // we last recorded
-		      if ((System.nanoTime() - st) >= 1000000000)
-		      {
-		         System.out.println("(FPS: "+fps+")");
-		         lastFpsTime = 0;
-		         fps = 0;
-		      }
-		      
-		   }
+		// keep looping round til the game ends
+		while (loop)
+		{
+			// work out how long its been since the last update, this
+			// will be used to calculate how far the entities should
+			// move this loop
+			long st = System.nanoTime();
+
+			try {
+				updateMap();
+			}
+			catch(Exception e) {
+				thread.stop();
+			}
+
+
+			while ((System.nanoTime() - st) < 1_000_000_000 / TARGET_FPS);
+
+			// update the frame counter
+			fps++;
+
+			// update our FPS counter if a second has passed since
+			// we last recorded
+			if ((System.nanoTime() - st) >= 1000000000)
+			{
+				System.out.println("(FPS: "+fps+")");
+				lastFpsTime = 0;
+				fps = 0;
+			}
+
 		}
+	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		int buttonPressed = e.getKeyCode();
 
-        if (buttonPressed == KeyEvent.VK_LEFT || buttonPressed == KeyEvent.VK_A) {
-            dx = -1;
-        }
+		if (buttonPressed == KeyEvent.VK_LEFT || buttonPressed == KeyEvent.VK_A) {
+			dx = -1;
+		}
 
-        if (buttonPressed == KeyEvent.VK_RIGHT || buttonPressed == KeyEvent.VK_D) {
-        	dx = 1;
-        }
+		if (buttonPressed == KeyEvent.VK_RIGHT || buttonPressed == KeyEvent.VK_D) {
+			dx = 1;
+		}
 
-        if (buttonPressed == KeyEvent.VK_UP || buttonPressed == KeyEvent.VK_W) {
-        	dy = -1;
-        }
+		if (buttonPressed == KeyEvent.VK_UP || buttonPressed == KeyEvent.VK_W) {
+			dy = -1;
+		}
 
-        if (buttonPressed == KeyEvent.VK_DOWN || buttonPressed == KeyEvent.VK_S) {
-        	dy = 1;
-        }
+		if (buttonPressed == KeyEvent.VK_DOWN || buttonPressed == KeyEvent.VK_S) {
+			dy = 1;
+		}
 	}
 
 	@Override
@@ -241,32 +240,30 @@ public class Map extends JPanel implements Runnable, KeyListener{
 		// TODO Auto-generated method stub
 		int buttonPressed = e.getKeyCode();
 
-        if (buttonPressed == KeyEvent.VK_LEFT || buttonPressed == KeyEvent.VK_A) {
-            dx = 0;
-        }
+		if (buttonPressed == KeyEvent.VK_LEFT || buttonPressed == KeyEvent.VK_A) {
+			dx = 0;
+		}
 
-        if (buttonPressed == KeyEvent.VK_RIGHT || buttonPressed == KeyEvent.VK_D) {
-        	dx = 0;
-        }
+		if (buttonPressed == KeyEvent.VK_RIGHT || buttonPressed == KeyEvent.VK_D) {
+			dx = 0;
+		}
 
-        if (buttonPressed == KeyEvent.VK_UP || buttonPressed == KeyEvent.VK_W) {
-        	dy = 0;
-        }
+		if (buttonPressed == KeyEvent.VK_UP || buttonPressed == KeyEvent.VK_W) {
+			dy = 0;
+		}
 
-        if (buttonPressed == KeyEvent.VK_DOWN || buttonPressed == KeyEvent.VK_S) {
-        	dy = 0;
-        }
-       
+		if (buttonPressed == KeyEvent.VK_DOWN || buttonPressed == KeyEvent.VK_S) {
+			dy = 0;
+		}
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 
 
 }
-	
-	
